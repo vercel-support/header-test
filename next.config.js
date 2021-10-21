67f7fbdeb0c25cb392a91b74ab3a39c22d2400d0
Riz <rizwana.akmal@hotmail.com>
2021-10-21 17:28:55 +0100

    add 'has'

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // needs testing
+        {
+          source: "/",
+          destination: "/",
+          has: [
+            { type: "header", key: "x-vicinity-cf-secret", value: "example" },
+          ],
+        },
+        {
+          source: "/:path*",
+          destination: "/:path*",
+          has: [
+            { type: "header", key: "x-vicinity-cf-secret", value: "example" },
+          ],
+        },
+        {
+          source: "/(.*)",
+          destination: "/no-header",
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
+};
---
+++ pages/no-header.js
@@ -0,0 +1,7 @@
+export default function noHeader() {
+  return (
+    <div>
+      <h1>No Header</h1>
+    </div>
+  );
+}
